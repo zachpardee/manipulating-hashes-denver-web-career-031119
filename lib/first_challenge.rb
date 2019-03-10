4e076d35765ee9f7bdac3fdd057763495061bcd2
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-10 20:13:29 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,6 +14,11 @@ def first_challenge
   }
 
   #your code here
+  contacts["Freddy Mercury"].each do |key,val|
+    if key == :favorite_icecream_flavors
+      val.delete_if {|ele| ele == "strawberry"}
+    end
+  end
 
 
   #remember to return your newly altered contacts hash!
